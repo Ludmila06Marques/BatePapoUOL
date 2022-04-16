f5f27c78169321a286c39818ad83cfffc4df6955
Lud <instimila@gmail.com>
2022-04-16 17:14:16 +0200

    Implementacao entrar na sala

--- scripts.js
+++ scripts.js
@@ -1,52 +1,101 @@
-
+/*
+PROBLEMAS:
+    -Como fazer o texto quebrar a linha quando chega no final da div
   
+*/
+
+document.addEventListener("keypress", function(e){
+    if(e.key==="Enter"){
+      
+       const btn=document.querySelector(".icone")
+       btn.click();
+      
+    }
+})
+
+
 //Entrar na sala
 function entrar(){
-    document.querySelector(".entrar").classList.add("escondido")
-    document.querySelector(".entrei").classList.remove("escondido")
-    let usuario = document.querySelector(".usuario-input").value
-    let pessoa={nome:usuario}
-    const promise =axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ',pessoa);
+    const usuario = document.querySelector(".usuario-input").value
+    const pessoa={name:usuario}
+    const promise =axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',pessoa);
+    console.log(promise)
     promise.then(entrarSucesso)
-    promise.catch(entrarErro)
+    promise.catch(entrarErro)    
 }
 
-        function entrarSucesso(){
-            console.log(alert("entrando..."))
+        function entrarSucesso(response){
+            const usuario = document.querySelector(".usuario-input").value
+            if(usuario!==response.data){
+                document.querySelector(".entrar").classList.add("escondido")
+                document.querySelector(".entrei").classList.remove("escondido")       
+               
+            }else{
+                entrarErro()
+            }
             carregarMensagens()
-            
+   
         }
         function entrarErro(error){
-            if(error.responde.status ===400){
-                alert("Usuario ja cadastrado. Tente outro nome!")
-                entrar();
+            if(error.response.status ==400){
+                const statusCode=error.response.status
+                console.log(statusCode)
+                alert("Usuario ja cadastrado. Tente outro nome!")             
             }
 
         }
+ /*   
+
+//Carregar mensagens        
+function carregarMensagens(){
+            alert("carregando mensagens")
+            const promise= axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',mensagem)
+            promise.then( enviarMensagemSucesso)
+            promise.catch( enviarMensagemErro)
+  
+        }
+               
+   
+    
 //Manter conexao
+//Manter conexao
+setInterval(manterConexao(),5000)
+function manterConexao(response){
+    const usuario = document.querySelector(".usuario-input").value
+    const pessoa={name:usuario}
+    const promise =axios.post('https://mock-api.driven.com.br/api/v6/uol/status',pessoa);
+    console.log(alert("Conectado"))
+
+}
 //setInterval(entrar,5000);
 
+
 //Carregar mensagens
-function carregarMensagens(){
-    const promise= axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
 
-}
-        function carregarMensagensSucesso(){
 
-        }
-        function carregarMensagensErro(){
-            
-        }
-
+ 
+ function carregarMensagensSucesso(){
+                    alert("carregando mensagens")
+                }
+                function carregarMensagensErro(){
+                    
+                    alert("deu ruim")
+                }  
 //Enviar mensagem
 function enviarMensagem(){
+    alert("enviando mensagens")
+ 
 
    const promise= axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',mensagem)
-
+   promise.then( enviarMensagemSucesso)
+   promise.catch( enviarMensagemErro)
 }
         function enviarMensagemSucesso(){
+            alert("enviando mensagens")
 
         }
         function enviarMensagemErro(){
             
         }
+//----------------------------------------------------------------------------------
+*/
